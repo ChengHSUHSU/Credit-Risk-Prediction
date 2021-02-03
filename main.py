7d781eeef5c68b6b6886843fc7c94cf7c2f51dea
ChengHSUHSU <76766937+ChengHSUHSU@users.noreply.github.com>
2021-02-04 02:13:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,3 @@
-
-
-
-'''
-1/31
-data load module
-data process module
-=> add standardize for lr / svm (3)
-model module
-=> add logistic regression[DONE] / svm / decision tree[DONE] / random forest[DONE] / adaboost[DONE] /  gbdt[DONE] / xgboost[DONE]
-=> add cross validation (4)
-evaluation module
-=> add eval metric such as accuracy / precision / recall / F1 [DONE]
-=> add eval plot such ROC (5)
-=> add explainable route (6)
-'''
-
 from load_data import Load_Data
 from data_process import Data_Processing
 from model import Model
@@ -34,14 +17,3 @@ if __name__ == '__main__':
     # evaluating
     evaluation_module  = Evaluation(model, test_credit_dataframe, test_target_dataframe, categories_feature, used_model = 'xgboost')
     evaluation_module.forward()
-
-
-
-
-
-
-
-
-
-
-
